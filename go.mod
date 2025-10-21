f87d3a1ce14222f2742e33b682493f1dfeb7d36c
Kayky <kaykyazevedo97@gmail.com>
2025-10-21 18:03:34 -0300

    feat: main.go

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/pupunha-code/Manicoba
 
 go 1.18
 
-require github.com/bwmarrin/discordgo v0.29.0
+require (
+	github.com/bwmarrin/discordgo v0.29.0
+	github.com/joho/godotenv v1.5.1
+)
 
 require (
 	github.com/gorilla/websocket v1.4.2 // indirect
--- main.go
+++ main.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/pupunha-code/Manicoba/articles"
+	"github.com/pupunha-code/Manicoba/bot"
+)
+
 func main() {
+
+	// Pega as variaveis de ambiente
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	token := os.Getenv("DISCORD_TOKEN")
+	channelID := os.Getenv("DISCORD_CHANNEL_ID")
+
+	bot.ArticleSender(bot.SessionCreator(token), channelID, articles.FetchMorningNews())
 }
